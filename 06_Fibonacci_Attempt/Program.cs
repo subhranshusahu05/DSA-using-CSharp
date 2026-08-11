1164152e441fcbd5036efe7b053f8833ecd8178f
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-11 22:26:10 +0530

    Add Fibonacci sequence solution

---
+++ 06_Fibonacci_Attempt/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _06_Fibonacci_Attempt
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 0 1 1 2 3 5 8 13 21 34 55 89......
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int current = 0;    
+
+            int prev = 1;
+
+            int last = 0;
+
+            for (int i = 1; i<=number; i++)
+            {
+                current = prev + last;
+
+                last = prev;
+                prev = current;
+
+                Console.WriteLine(current);
+
+
+            }
+
+
+
+
+        }
+    }
+}
