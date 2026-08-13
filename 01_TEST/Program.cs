3b1d7e321dbf4efae88d781f2744e6d38325e4ef
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-13 07:41:13 +0530

    Add pattern problems folder and first project

---
+++ 01_TEST/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01_TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("jUST TESTING THE FILE LOCATION");
+           
+        }
+    }
+}
