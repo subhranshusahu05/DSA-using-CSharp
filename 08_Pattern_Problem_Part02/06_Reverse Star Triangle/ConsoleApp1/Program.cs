ab0fff897cd2eac4afa8573382b5913ade7fc757
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-17 18:48:34 +0530

    Reversed Star pattern added

---
+++ 08_Pattern_Problem_Part02/06_Reverse Star Triangle/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    /*
+     
+     *****
+     ****
+     ***
+     **
+     *
+     
+     
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }    
+        }
+    }
+}
