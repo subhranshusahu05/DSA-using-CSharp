bf3bad6c22eeeb6dceeff5f4f276507f08a7d96a
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-21 22:03:05 +0530

    *    **   ***  **** *****

---
+++ 09_NumberPattern_HardProblems/01_star_print/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+
+namespace _01_star_print
+{
+    /*
+    
+
+    *
+   **
+  ***
+ ****
+*****
+    
+    
+    
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i =1; i<=5; i++)
+            {
+                //blank print
+                for (int j=1;j<=5-i;j++)
+                {
+                    Console.Write(" ");
+
+                }
+
+                //Print star
+
+                for (int j=1;j<=i;j++)
+                {
+                    Console.Write("*");
+
+                }
+
+                Console.WriteLine();
+
+
+            }
+
+
+        }
+    }
+}
