1e8b5922fde0864c3255bf312e91c38d72c4ba46
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-21 22:12:10 +0530

    Number_Pattern
        1
       22
      333
     4444
    55555

---
+++ 09_NumberPattern_HardProblems/02_Number_Pattern/number/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace number
+{
+    internal class Program
+    {
+        /*
+         
+                1
+               22
+              333
+             4444
+            55555
+         
+         
+         
+         */
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                //blank print
+                for (int j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write(" ");
+
+                }
+
+                //Print star
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+
+                }
+
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
