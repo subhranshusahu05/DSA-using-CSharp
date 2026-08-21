8bf9d60d8b19c1380fbba539cad246fa013c78d0
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-21 22:27:41 +0530

    1    12   123  1234 12345

---
+++ 09_NumberPattern_HardProblems/03_number_Pattern/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{/*
+  
+
+    1
+   12
+  123
+ 1234
+12345
+  
+  
+  
+  
+  */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 5; i++)
+            {
+                //blank print
+                for (int j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write(" ");
+
+                }
+
+                //Print star
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+
+                }
+
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
