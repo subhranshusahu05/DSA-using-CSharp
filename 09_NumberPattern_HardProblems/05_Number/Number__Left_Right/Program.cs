77b88555388b847bb14e07817862b3253214b714
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-23 14:53:43 +0530

    Adding_Problems             1            21           321          4321         54321

---
+++ 09_NumberPattern_HardProblems/05_Number/Number__Left_Right/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Number__Left_Right
+{
+    internal class Program
+    {
+        /*
+         
+
+            1
+           21
+          321
+         4321
+        54321
+         
+         
+         
+         */
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i-j+1);
+
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
