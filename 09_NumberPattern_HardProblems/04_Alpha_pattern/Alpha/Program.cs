2268e83afa4f09e027d402967508737ae8653551
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-23 14:40:40 +0530

    Adding New question solution             A            AB           ABC          ABCD         ABCDE

---
+++ 09_NumberPattern_HardProblems/04_Alpha_pattern/Alpha/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Alpha
+{
+    internal class Program
+    {
+        /*
+            A
+           AB
+          ABC
+         ABCD
+        ABCDE
+         
+         
+         
+         */
+        static void Main(string[] args)
+        {
+            for (int i=1;i<=5;i++)
+            {
+                for (int j=1;j<=5-i;j++)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int j= 1;j<=i;j++)
+                {
+                    Console.Write((char)(65+j-1));
+
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
