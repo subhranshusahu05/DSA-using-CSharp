6cb4959bb5f103ccb6fbce45d114fb6479771af6
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-23 18:41:09 +0530

    NumberConversion_DecialToBinary

---
+++ 10_NumberSystem/01_DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DecimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int ans = 0;
+            int rem = 0;
+            int mul = 1;
+
+            while (num>0)
+            {
+                rem = num % 2;
+               // Console.WriteLine("The reminber is"+rem);
+
+                ans = ans + rem * mul;
+
+              //  Console.WriteLine("The Answer is"+ans);
+                mul = mul * 10;
+
+              //  Console.WriteLine("The mul is"+mul);
+                num = num / 2;
+
+              //  Console.WriteLine("The number is"+num);
+
+            }
+            Console.WriteLine("The actual answer is "+ans);
+
+
+        }
+    }
+}
