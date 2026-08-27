9dbe7bebf2530fec4fbe3050201c492ae77be12a
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-27 20:46:12 +0530

    Binary_TO_Decimal_Convert

---
+++ 10_NumberSystem/02_BinaryToDecimal/Test_My/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Test_My
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter binary number: ");
+            string ? binary = Console.ReadLine();
+            int totalSum = 0;
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                int digit = binary[i] - '0';
+                int power = binary.Length - 1 - i;
+                int product = digit * (int)Math.Pow(2, power);
+
+                Console.WriteLine($"{digit} × 2^{power} = {product}");
+                totalSum += product;
+            }
+
+            Console.WriteLine($"Total: {totalSum}");
+
+        }
+    }
+}
