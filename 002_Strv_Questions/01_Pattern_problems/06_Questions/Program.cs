23a1296c6f803dbc0fb1593656ef0c0a5e0a1c4f
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-30 00:54:06 +0530

    Addint_Striver_     *    ***   *****  ******* *********

---
+++ 002_Strv_Questions/01_Pattern_problems/06_Questions/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _06_Questions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =5;i>=1; i--)
+            {
+                for (int j= 1; j<=i; j++)
+                {
+                    Console.Write(j);
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 002_Strv_Questions/01_Pattern_problems/07_Questions/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _07_Questions
+{
+    /*
+     
+    *
+   ***
+  *****
+ *******
+*********
+     
+     
+     
+     
+     
+     */
+    internal class Program
+    {
+        /*
+         
+         
+    *
+   ***
+  *****
+ *******
+*********
+         
+         
+         
+         
+         
+         */
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int p = 1; p <= 5-i; p++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int p = 1; p <= i-1 ; p++)
+                {
+                    Console.Write("*");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
