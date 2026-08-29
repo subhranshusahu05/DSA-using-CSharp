b3671c39b8eb66823c7d2b86df1ebf1d0caa02ff
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-29 20:31:34 +0530

    Leap_year_question_added

---
+++ 001_LeetCode_Questions/02_Rohit_Questions/01___258_2_7_231_69_9/02__2. Check a year is Leap Year or not/Leap_year_Function/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Leap_year_Function
+{
+    internal class Program
+    {
+        // function make
+        static bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0) { return true; }
+            else if (year % 4 == 0 && year % 100! == 0) { return true; }
+            else
+            {
+
+                return false;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+
+            int year = Convert.ToInt32(Console.ReadLine());
+            bool isLeap = IsLeapYear(year);
+
+            if (isLeap)
+            {
+                Console.WriteLine(year + " is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year.");
+
+
+
+            }
+
+
+
+        }
+    }
+}
---
+++ 001_LeetCode_Questions/02_Rohit_Questions/01___258_2_7_231_69_9/02__2. Check a year is Leap Year or not/Leap_year_question/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Leap_year_question
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 400 ==0)
+            {
+                Console.WriteLine(year + " is a leap year.");
+
+            }
+            else if (year % 4 ==0 && year % 100 !=0)
+            {
+                Console.WriteLine(year + " is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year.");
+            }
+        }
+    }
+}
