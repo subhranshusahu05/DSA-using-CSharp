4b21cdaaa6ca4c93d786cd3db10cd20a96636f1a
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-29 19:14:34 +0530

    adding test

--- 001_LeetCode_Questions/02_Rohit_Questions/01___258_2_7_231_69_9/01__258. Add Digits/258. Add Digits/Program.cs
+++ 001_LeetCode_Questions/02_Rohit_Questions/01___258_2_7_231_69_9/01__258. Add Digits/258. Add Digits/Program.cs
@@ -57,7 +57,155 @@ namespace _258._Add_Digits
             Console.WriteLine("Single digit result: " + num);
         }
 
-        
+
     }
 }
 
+/*
+ 
+ 
+
+
+
+namespace _258._Add_Digits
+{
+    internal class Program
+    {
+        
+
+        static int AddDigits(int num)
+        {
+            while (num > 9)
+            {
+                int rem = 0;
+                int ans = 0;
+
+                while (num != 0)
+                {
+                    rem = num % 10;
+                    num = num / 10;
+                    ans = ans + rem;
+                }
+
+                num = ans;
+            }
+
+            return num;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int result = AddDigits(num);
+
+            Console.WriteLine("Single digit result: " + result);
+        }
+
+
+
+    }
+}
+
+
+
+
+
+ 
+ 
+ 
+ 
+ */
+
+
+
+
+/*
+//namespace _258._Add_Digits
+//{
+//    internal class Program
+//    {
+//        /*
+//            NON-STATIC METHOD
+
+//            AddDigits() is a non-static method because we have
+//            NOT used the 'static' keyword.
+
+//            Therefore, we need to create an object of the
+//            Program class before calling AddDigits().
+//        */
+
+//public int AddDigits(int num)
+//{
+//    while (num > 9)
+//    {
+//        int rem = 0;
+//        int ans = 0;
+
+//        while (num != 0)
+//        {
+//            rem = num % 10;
+//            num = num / 10;
+//            ans = ans + rem;
+//        }
+
+//        num = ans;
+//    }
+
+//    return num;
+//}
+
+
+//static void Main(string[] args)
+//{
+//    Console.Write("Enter a number: ");
+//    int num = Convert.ToInt32(Console.ReadLine());
+
+
+//    /*
+//        OBJECT CREATION
+
+//        AddDigits() is non-static.
+
+//        Main() is static.
+
+//        A static method cannot directly call a
+//        non-static method.
+
+//        Therefore, we create an object of Program class.
+
+//        Syntax:
+
+//            ClassName objectName = new ClassName();
+//    */
+
+//    Program obj = new Program();
+
+
+//    /*
+//        CALLING NON-STATIC METHOD
+
+//        We use the object 'obj' to call AddDigits().
+
+//        obj → object of Program class
+//        AddDigits() → non-static method
+//    */
+
+//    int result = obj.AddDigits(num);
+
+
+//    // Display the result
+//    Console.WriteLine("Single digit result: " + result);
+//}
+//    }
+//}
+
+
+
+
+
+
+
+//*/
---
+++ 001_LeetCode_Questions/02_Rohit_Questions/01___258_2_7_231_69_9/01__258. Add Digits/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Test
+{
+    public class Program1 
+    {    
+       public int AddDigits(int num)
+       {
+            while (num > 9)
+            {
+                int rem = 0;
+                int ans = 0;
+
+                while (num != 0)
+                {
+                    rem = num % 10;
+                    num = num / 10;
+                    ans = ans + rem;
+                }
+
+                num = ans;
+            }
+
+            return num;
+       }
+    }
+    public class program2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Program1 obj = new Program1();
+            int result = obj.AddDigits(num);
+            Console.WriteLine("Single digit result: " + result);
+        }
+    }
+}
